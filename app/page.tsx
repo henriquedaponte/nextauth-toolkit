4b404499b135447d52577aee457f3e6414826a13
henriquedaponte <henriquedaponte@gmail.com>
2024-08-16 15:30:54 -0300

    changed background

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,10 @@ import { Button } from '@/components/ui/button'
 
 export default function Home() {
   return (
-   <Button>Click Me</Button>
+   <main className="flex h-full flex-col 
+   items-center justify-center 
+   bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800"> 
+   
+   </main>
   )
 }
