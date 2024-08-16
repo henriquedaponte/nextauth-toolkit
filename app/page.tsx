af74023e34ddd33effc9b8cda198d49576581d39
henriquedaponte <henriquedaponte@gmail.com>
2024-08-16 15:45:52 -0300

    styling front page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,32 @@
-import { Button } from '@/components/ui/button'
+import { Poppins } from "next/font/google"
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"]
+})
 
 export default function Home() {
   return (
    <main className="flex h-full flex-col 
    items-center justify-center 
-   bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800"> 
-   
+   bg-blue-500">
+    <div className='space-y-6'>
+      <h1 className={cn(
+        'text-6xl font-semibold text-white drop-shadow-md text-center',
+        font.className,)}>
+        🔐 Auth
+      </h1>
+      <p className='text-white text-large'>
+        A simple authentication service
+      </p>
+        <div>
+          <Button variant="secondary" size="lg">
+            Sign In
+          </Button>
+        </div>
+      </div> 
    </main>
   )
 }
