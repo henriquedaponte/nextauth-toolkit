d5a720c86cad0ce5ba31636c1eb985f2cc6949f3
henriquedaponte <henriquedaponte@gmail.com>
2024-08-16 16:09:55 -0300

    started working on login button for auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { Poppins } from "next/font/google"
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
+import { LoginButton } from "@/components/auth/login-button"
 
 const font = Poppins({
   subsets: ["latin"],
@@ -9,24 +10,26 @@ const font = Poppins({
 
 export default function Home() {
   return (
-   <main className="flex h-full flex-col 
+    <main className="flex h-full flex-col 
    items-center justify-center 
    bg-blue-500">
-    <div className='space-y-6'>
-      <h1 className={cn(
-        'text-6xl font-semibold text-white drop-shadow-md text-center',
-        font.className,)}>
-        🔐 Auth
-      </h1>
-      <p className='text-white text-large'>
-        A simple authentication service
-      </p>
+      <div className='space-y-6'>
+        <h1 className={cn(
+          'text-6xl font-semibold text-white drop-shadow-md text-center',
+          font.className,)}>
+          🔐 Auth
+        </h1>
+        <p className='text-white text-large'>
+          A simple authentication service
+        </p>
         <div>
-          <Button variant="secondary" size="lg">
-            Sign In
-          </Button>
+          <LoginButton>
+            <Button variant="secondary" size="lg">
+              Sign In
+            </Button>
+          </LoginButton>
         </div>
-      </div> 
-   </main>
+      </div>
+    </main>
   )
 }
