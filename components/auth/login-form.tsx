f08bfda008e915fe02813ee6c6ee314a8b68833d
henriquedaponte <henriquedaponte@gmail.com>
2024-08-21 16:08:56 -0300

    added email form field

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,14 +1,65 @@
-import { CardWrapper } from "./card-wrapper"
+"use client"
+
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage
+} from '@/components/ui/form';
+import { LoginSchema } from "@/schemas";
+import { CardWrapper } from "./card-wrapper";
+import { Input } from "../ui/input";
 
 export const LoginForm = () => {
+    const form = useForm<z.infer<typeof LoginSchema>>({
+        resolver: zodResolver(LoginSchema),
+        defaultValues: {
+            email: "",
+            password: ""
+        },
+    })
     return (
         <CardWrapper
-        headerLabel="Welcome Back"
-        backButtonLabel="Don't have an account?"
-        backButtonHref="/auth/register"
-        showSocial
+            headerLabel="Welcome Back"
+            backButtonLabel="Don't have an account?"
+            backButtonHref="/auth/register"
+            showSocial
         >
-            Login Form
+            <Form {...form}>
+                <form 
+                onSubmit={form.handleSubmit(() => {})}
+                className="space-y-6"
+                >
+
+                    <div className="space-y-4">
+                        <FormField
+                        control={form.control}
+                        name="email"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>
+                                    Email
+                                </FormLabel>
+                                <FormControl>
+                                    <Input
+                                    {...field}
+                                    placeholder="john.doe@example.com"
+                                    type="email"
+                                    />
+                                </FormControl>
+                            </FormItem>
+                        )}
+                        />
+                    </div>
+
+                </form>
+            </Form>
         </CardWrapper>
     )
 }
